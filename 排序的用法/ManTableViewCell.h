a62de7264bcefa29e47444d2d4045a586e59f7cc
刘宇帅 <CesareBorgia@MacBook-Pro.local>
2018-07-30 16:25:53 +0800

    XIB视图

---
+++ 排序的用法/ManTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ManTableViewCell.h
+//  排序的用法
+//
+//  Created by 刘宇帅 on 2018/7/30.
+//  Copyright © 2018年 牛孟强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ManTableViewCell : UITableViewCell
+
+@end
---
+++ 排序的用法/ManTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ManTableViewCell.m
+//  排序的用法
+//
+//  Created by 刘宇帅 on 2018/7/30.
+//  Copyright © 2018年 牛孟强. All rights reserved.
+//
+
+#import "ManTableViewCell.h"
+
+@implementation ManTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
